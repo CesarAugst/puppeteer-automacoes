4936a2bbcd62f09c1cbfcb2fc661316c473411da
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2023-08-22 10:31:34 -0300

    desenvolvimento do modulo de whatsapp

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
+/*CONSTANTES DE VALOR*/
+const EXISTS_NUMBER = `5511958681942`;
+const NOM_EXISTS_NUMBER = `5511958681945`;
+const DATA_DIR = `./data_dir`;
+
+/*IMPORTACOES*/
 const puppeteer = require("puppeteer");
 
-(async () => {
-    const browser = await puppeteer.launch({
-        headless: false
-    });
-    const page = await browser.newPage();
-    await page.goto('https://example.com');
-    await page.screenshot({path: './assets/example.png'})
+/*FUNCOES UTILITARIAS*/
+const { delay } = require('./src/utils/f_delay.js');
 
-    await browser.close();
+(async () => {
+    //instancia o navegador
+    const browser = await puppeteer.launch({
+        userDataDir: DATA_DIR,
+        headless: false,
+    });
+    //abre uma nova aba
+    const page = await browser.newPage();
+    //navega para a pagina
+    await page.goto(`https://wa.me/${NOM_EXISTS_NUMBER}`);
+    //tita print
+    await page.screenshot({path: './assets/example.png'})
+    //clica no botao de de iniicar conversa
+    await page.click(`#action-button`)
+    //aguarda um tmepo antes de fechar o navegador
+    // await delay(2000);
+    //fecha navegador
+    // await browser.close();
 })();
---
+++ src/utils/env.js
@@ -0,0 +1,5 @@
+//desc: constantes que sao reutilziadas pela aplicacao
+const consts = {
+    linkedin_url: `https://www.linkedin.com/in/cesar-august/`
+};
+module.exports = consts;
