3725df78617bbe5fb3bbfb29f1ee48cdcef1c3b6
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2023-08-22 10:55:17 -0300

    reestruturação do whatsapp

--- src/modules/whatsapp.js
+++ src/modules/whatsapp.js
@@ -1,24 +1,24 @@
-/*CONSTANTES DE VALOR*/
-const EXISTS_NUMBER = `5511958681942`;
-const NOM_EXISTS_NUMBER = `5511958681945`;
-const DATA_DIR = `./data_dir`;
-
 /*IMPORTACOES*/
 const puppeteer = require("puppeteer");
 
 /*FUNCOES UTILITARIAS*/
 const { delay } = require('../utils/f_delay.js');
+const { CONSTS } = require('../../env.js');
+
+/*CONSTANTES DE VALOR*/
+const CONST = CONSTS();
 
 (async () => {
+
     //instancia o navegador
     const browser = await puppeteer.launch({
-        userDataDir: DATA_DIR,
+        userDataDir: CONST.DATA_DIR,
         headless: false,
     });
     //abre uma nova aba
     const page = await browser.newPage();
     //navega para a pagina
-    await page.goto(`https://wa.me/${NOM_EXISTS_NUMBER}`);
+    await page.goto(`https://wa.me/${CONST.NOM_EXISTS_NUMBER}`);
     //tita print
     await page.screenshot({path: './assets/example.png'})
     //clica no botao de de iniicar conversa
@@ -27,4 +27,5 @@ const { delay } = require('../utils/f_delay.js');
     // await delay(2000);
     //fecha navegador
     // await browser.close();
+
 })();
