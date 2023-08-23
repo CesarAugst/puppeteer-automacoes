54ee071a89346209e03ddf6a8b5ec20ea4b04461
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2023-08-23 10:49:02 -0300

    usando proxy na requisicao

--- src/modules/linkedin.js
+++ src/modules/linkedin.js
@@ -1,17 +1,55 @@
 /*IMPORTACOES*/
-const puppeteer = require("puppeteer");
-const { Cluster } = require("puppeteer-cluster")
+const vanillaPuppeteer = require('puppeteer');
+const { addExtra } = require('puppeteer-extra');
+const { Cluster } = require("puppeteer-cluster");
+const Stealth = require('puppeteer-extra-plugin-stealth');//permitou rastrear sites com cloud flare protection habilitado
+const randomUseragent = require('random-useragent');
 
 /*FUNCOES UTILITARIAS*/
 const { delay } = require('../utils/f_delay.js');
 const { CONSTS } = require('../../env.js');
 
 /*CONSTANTES DE VALOR*/
+const proxy = 'zproxy.lum-superproxy.io:22225';
+const username = 'lum-customer-hl_492cf951-zone-brasil';
+const password = 'q2k0zn2fabn4';
 const CONST = CONSTS();
-
+const options = {
+    headless: false,
+    userDataDir: CONST.DATA_DIR,
+    args: [
+        `--proxy-server=${proxy}`,
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-infobars',
+        '--no-zygote',
+        '--no-first-run',
+        '--window-size=1920x1080',
+        '--window-position=0,0',
+        '--ignore-certificate-errors',
+        '--ignore-certificate-errors-skip-list',
+        '--disable-dev-shm-usage',
+        '--disable-accelerated-2d-canvas',
+        '--disable-gpu',
+        '--hide-scrollbars',
+        '--disable-notifications',
+        '--disable-background-timer-throttling',
+        '--disable-backgrounding-occluded-windows',
+        '--disable-breakpad',
+        '--disable-component-extensions-with-background-pages',
+        '--disable-extensions',
+        '--disable-features=TranslateUI,BlinkGenPropertyTrees',
+        '--disable-ipc-flooding-protection',
+        '--disable-renderer-backgrounding',
+        '--enable-features=NetworkService,NetworkServiceInProcess',
+        '--force-color-profile=srgb',
+        '--metrics-recording-only',
+        '--mute-audio',
+    ]
+};
 async function consulta_simple(){
     //instancia o navegador
-    const browser = await puppeteer.launch({
+    const browser = await vanillaPuppeteer.launch({
         userDataDir: CONST.DATA_DIR,
         headless: 'new'
     });
@@ -42,14 +80,14 @@ async function consulta_simple(){
 }
 
 async function consulta_batch({url_list}){
+    const puppeteer = addExtra(vanillaPuppeteer);
+    puppeteer.use(Stealth());
     //instancia o navegador
     const cluster = await Cluster.launch({
-        puppeteerOptions: {
-            headless: false,
-            userDataDir: CONST.DATA_DIR,
-        },
+        puppeteer,
         concurrency: Cluster.CONCURRENCY_PAGE,
         maxConcurrency: 15,
+        puppeteerOptions: options
     });
 
     //onde sao armazenadas as respostas
@@ -57,8 +95,33 @@ async function consulta_batch({url_list}){
 
     //o que cada pagina ira executar
     await cluster.task(async ({ page, data: url }) =>{
+
+        await page.setExtraHTTPHeaders({
+            'Accept-Language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7',
+            'Accept-Encoding': 'gzip, deflate',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
+        });
+        // randomizando view port pra evitar ser bloqueado
+        await page.setViewport({
+            width: 1920 + Math.floor(Math.random() * 100),
+            height: 3000 + Math.floor(Math.random() * 100),
+            deviceScaleFactor: 1,
+            hasTouch: false,
+            isLandscape: false,
+            isMobile: false,
+        });
+        // randomizando user agent pra evitar ser bloqueado
+        const UA = randomUseragent.getRandom(function (ua) {
+            return (ua.deviceType === '') && parseFloat(ua.browserVersion) >= 20;
+        });
+        await page.setUserAgent(UA);
+
+        // Authenticate our proxy with username and password defined above
+        await page.authenticate({ username, password });
+
         //navega para a pagina
         await page.goto(url);
+
 
         //executa js no contexto da pagina e retorna informacoes
         const data = await page.evaluate(() => {
@@ -67,6 +130,7 @@ async function consulta_batch({url_list}){
 
             //inicia tentativa de obtencao de dados
             try{
+
                 //busca pela imagem que contem o preview do usuario
                 const img = document.querySelectorAll('img.evi-image.ember-view')
                 //filtra apenas as informacoes desejadas
